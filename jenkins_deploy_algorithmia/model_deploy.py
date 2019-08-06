527acf17e2d9b181efb157a3b86ca99c064a2782
Jon Peck <peckjon@gmail.com>
2019-08-06 14:46:25 -0700

    DS-368 CI/CD Pipeline Integration

--- jenkins_deploy_algorithmia/model_deploy.py
+++ jenkins_deploy_algorithmia/model_deploy.py
@@ -1,6 +1,7 @@
 import Algorithmia
-import os
 from datetime import datetime
+import os
+import shutil
 
 api_key = os.environ.get('ALGORITHMIA_MANAGEMENT_API_KEY')
 username = os.environ.get('ALGORITHMIA_USERNAME')
@@ -11,29 +12,41 @@ if not username:
     raise SystemExit('Please set the evironment variable ALGORITHMIA_USERNAME')
 
 algo_name = username+'/digit_recognition_'+datetime.now().strftime('%Y%m%d%H%M%S')
+data_path = 'data://'+username+'/digit_recognition'
+
 client=Algorithmia.client(api_key)
 algo = client.algo(algo_name)
 
+print('CREATING '+algo_name)
 create_result = algo.create(
     details = {
-        "label": "Digit Recognition",
+        'label': 'Digit Recognition',
     },
     settings = {
-        "language": "python3-1",
-        "source_visibility": "closed",
-        "license": "apl",
-        "network_access": "full",
-        "pipeline_enabled": True,
-        "environment": "cpu"
+        'language': 'python3-1',
+        'source_visibility': 'closed',
+        'license': 'apl',
+        'network_access': 'full',
+        'pipeline_enabled': True,
+        'environment': 'cpu'
     }
 )
 print(create_result)
 
+print('CREATING '+data_path)
+if not client.dir(data_path).exists():
+    client.dir(data_path).create()
+
+print('UPLOADING model')
+client.file(data_path+'/digits.classifier.pkl').putFile('jenkins_deploy_algorithmia/digits_classifier.pkl')
+
+os.makedirs('temp', exist_ok = True)
 algo.compile()
 
+print('PUBLISHING '+algo_name)
 publish_result=algo.publish(
     version_info = {
-        "sample_input": "world"
+        'sample_input': 'world'
     }
 )
 print(publish_result)
