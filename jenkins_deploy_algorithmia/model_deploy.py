7b7084d666a2da4f416e3d1ab803e604a6ed54c0
Jon Peck <peckjon@gmail.com>
2019-08-06 17:01:40 -0700

    DS-368 CI/CD Pipeline Integration

--- jenkins_deploy_algorithmia/model_deploy.py
+++ jenkins_deploy_algorithmia/model_deploy.py
@@ -1,7 +1,9 @@
 import Algorithmia
 from datetime import datetime
+from git import Repo
 import os
-import shutil
+from shutil import copyfile
+import urllib.parse
 
 api_key = os.environ.get('ALGORITHMIA_MANAGEMENT_API_KEY')
 username = os.environ.get('ALGORITHMIA_USERNAME')
@@ -40,13 +42,31 @@ if not client.dir(data_path).exists():
 print('UPLOADING model')
 client.file(data_path+'/digits.classifier.pkl').putFile('jenkins_deploy_algorithmia/digits_classifier.pkl')
 
+print('CLONING repo')
 os.makedirs('temp', exist_ok = True)
-algo.compile()
+encoded_api_key = urllib.parse.quote_plus(api_key)
+algo_repo = "https://{}:{}@git.algorithmia.com/git/{}.git".format(username, encoded_api_key, algo_name)
+cloned_repo = Repo.clone_from(algo_repo, 'temp')
+
+print('ADDING algorithm files')
+algorithm_file_name='{}.py'.format(algo_name.split('/')[1])
+copyfile('jenkins_deploy_algorithmia/requirements.txt', 'temp/requirements.txt')
+with open('jenkins_deploy_algorithmia/algo.py', 'r+') as file_in:
+    with open('temp/src/'+algorithm_file_name, 'w+') as file_out:
+        filedata = file_in.read()
+        filedata = filedata.replace('data://username/demo/digits_classifier.pkl', data_path+'/digits.classifier.pkl')
+        file_out.write(filedata)
+cloned_repo.git.add(update=True)
+cloned_repo.index.commit("Add algorithm files")
+
+print('PUSHING repo')
+origin = cloned_repo.remote(name='origin')
+origin.push()
 
 print('PUBLISHING '+algo_name)
 publish_result=algo.publish(
     version_info = {
-        'sample_input': 'world'
+        'sample_input': 'https://github.com/algorithmiaio/sample-apps/blob/master/algo-dev-demo/digit_recognition/images/digit_1.png?raw=true'
     }
 )
 print(publish_result)
